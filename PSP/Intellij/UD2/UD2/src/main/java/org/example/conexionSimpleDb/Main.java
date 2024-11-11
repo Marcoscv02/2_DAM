5a81f93a6ef9fb080c550958195603334f432218
dam2 - Caetano Vázquez <A24MarcosCV@sanclemente.local>
2024-11-11 10:29:32 +0100

    11/11/2024
    AD

---
+++ PSP/Intellij/UD2/UD2/src/main/java/org/example/conexionSimpleDb/Main.java
@@ -0,0 +1,30 @@
+package org.example.conexionSimpleDb;
+
+import java.sql.*;
+
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        //Conexion SQlite
+        Connection con= DriverManager.getConnection("jdbc:sqlite:C:\\Users\\a24marcoscv\\Documents\\2_DAM\\ACCESO A DATOS\\UD2\\db\\zoo.db");
+        //Conexion h2
+        //Connection con = DriverManager.getConnection("jdbc:h2:C:\\Users\\a24marcoscv\\Documents\\2_DAM\\ACCESO A DATOS\\UD2\\db\\h2\\zoo");
+        System.out.println("Conexion establecida");
+
+        Statement st= con.createStatement();
+
+//        st.execute("DROP TABLE IF EXISTS Animal");
+//
+//        st.execute("CREATE TABLE Animal ("
+//                + "idAnimal INTEGER PRIMARY KEY, "
+//                + "idEspecie integer REFERENCES Especie (idEspecie), "
+//                + "nome VARCHAR(255),"
+//                + "dataNacemento TIMESTAMP)");
+
+        ResultSet rs= st.executeQuery("Select * From Especie");
+
+        while (rs.next()){
+            System.out.println("Especie "+rs.getInt(1)+" "+rs.getString(2)+" "+rs.getDouble(3));
+        }
+    }
+}
