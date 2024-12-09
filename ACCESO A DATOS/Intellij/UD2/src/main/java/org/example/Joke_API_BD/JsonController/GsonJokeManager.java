a33e3bc7df05e44bfbfe7e12f97c16b366f34b3f
dam2 - Caetano Vázquez <A24MarcosCV@sanclemente.local>
2024-12-09 13:03:44 +0100

    9/12/2024
    inti

---
+++ ACCESO A DATOS/Intellij/UD2/src/main/java/org/example/Joke_API_BD/JsonController/GsonJokeManager.java
@@ -0,0 +1,44 @@
+package org.example.Joke_API_BD.JsonController;
+
+import com.google.gson.*;
+import org.example.Joke_API_BD.Chiste;
+
+import java.lang.reflect.Type;
+
+public class GsonJokeManager {
+
+    public static final String API_URL = "https://sv443.net/jokeapi/v2/joke/";
+
+    private Gson gson;
+
+    private static GsonJokeManager instance;
+
+    private GsonJokeManager() {
+        gson = new GsonBuilder()
+                .registerTypeAdapter(Chiste.class, new JsonDeserializer<Chiste>() {
+                    @Override
+                    public Chiste deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+                        return null;
+                    }
+                })
+                .setPrettyPrinting().create();
+    }
+
+    public static GsonJokeManager getInstance() {
+        if (instance == null) {
+            synchronized (GsonJokeManager.class) {
+                if (instance == null) {
+                    instance = new GsonJokeManager();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Gson getGson() {
+        return gson;
+    }
+
+
+
+}
