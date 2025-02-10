42252364eae75268f31993bef88d8d5040da4296
dam2 - Caetano Vázquez <A24MarcosCV@sanclemente.local>
2025-02-10 10:31:27 +0100

    10/02/25

--- ACCESO A DATOS/Intellij/UD3/UD3/src/main/java/marcos/videojuegos/model/Imagen.java
+++ ACCESO A DATOS/Intellij/UD3/UD3/src/main/java/marcos/videojuegos/model/Imagen.java
@@ -1,4 +1,63 @@
 package marcos.videojuegos.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Imagen {
+    @Id
+    private Long IdImagen;
+    private String urlImage;
+    private Byte[] image;
+
+    @ManyToOne
+    @JoinColumn(name = "imagenes")
+    private Long idJuego;
+
+    public Imagen() {
+    }
+
+    public Imagen(Long idImagen, String urlImage, Byte[] image) {
+        IdImagen = idImagen;
+        this.urlImage = urlImage;
+        this.image = image;
+    }
+
+    public Imagen(Long idImagen, String urlImage, Byte[] image, Long idJuego) {
+        IdImagen = idImagen;
+        this.urlImage = urlImage;
+        this.image = image;
+        this.idJuego = idJuego;
+    }
+
+    public Long getIdImagen() {
+        return IdImagen;
+    }
+
+    public void setIdImagen(Long idImagen) {
+        IdImagen = idImagen;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Long getIdJuego() {
+        return idJuego;
+    }
+
+    public void setIdJuego(Long idJuego) {
+        this.idJuego = idJuego;
+    }
 }
--- ACCESO A DATOS/Intellij/UD3/UD3/src/main/java/marcos/videojuegos/model/Juego.java
+++ ACCESO A DATOS/Intellij/UD3/UD3/src/main/java/marcos/videojuegos/model/Juego.java
@@ -1,4 +1,27 @@
 package marcos.videojuegos.model;
 
+import jakarta.persistence.*;
+import marcos.videojuegos.model.plataforma.PataformaConverter;
+import marcos.videojuegos.model.plataforma.Plataforma;
+
+import java.util.List;
+
+@Entity
 public class Juego {
+    @Id
+    private Long idJuego;
+
+    @ManyToMany
+    @JoinTable( name = "generoJuego",
+                joinColumns = @JoinColumn(name = "idGenero"),
+                inverseJoinColumns = @JoinColumn(name = "idGenero"))
+    private List<Genero>generos;
+
+    @OneToMany(mappedBy = "IdImagen")
+    private List<Imagen>imagenes;
+
+    @Convert(converter = PataformaConverter.class)
+    private Plataforma plataforma;
+
+    private RequisitosSistema requisitos;
 }
