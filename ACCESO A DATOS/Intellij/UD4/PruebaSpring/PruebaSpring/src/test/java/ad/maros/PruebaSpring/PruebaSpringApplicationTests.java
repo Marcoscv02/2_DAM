7b8680b967d6b30eb92fedf22ed3bb761ad52159
Marcoscv02 <marcoscvwork@gmail.com>
2025-02-28 12:57:25 +0100

    aaaaa

---
+++ ACCESO A DATOS/Intellij/UD4/PruebaSpring/PruebaSpring/src/test/java/ad/maros/PruebaSpring/PruebaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ad.maros.PruebaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
